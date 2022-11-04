2d95c3147b902289ce75a88bdc6b92457f7d49b4
Sergiotaap <Sergiotapiarodri@gmail.com>
2022-11-04 17:50:17 +0100

    Se añaden modificaciones al archivo 16 js

--- Javascript/ej16.js
+++ Javascript/ej16.js
@@ -31,5 +31,15 @@ function llamada(){
                 document.getElementById('tipo').innerHTML=data.types[0].type.name
             ));
         break;
+        case 'Starly':
+            fetch('https://pokeapi.co/api/v2/pokemon/starly')
+            .then(res => res.json())
+            .then((data)=>(
+                document.getElementById('nombre').innerHTML=data.name,
+                document.getElementById('habilidades').innerHTML=data.abilities[0].ability.name+", "+data.abilities[1].ability.name,
+                document.getElementById('stats').innerHTML=data.stats[0].stat.name+" "+data.stats[0].base_stat+" / "+data.stats[1].stat.name+" "+data.stats[1].base_stat+" / "+data.stats[2].stat.name+" "+data.stats[2].base_stat+" / "+data.stats[3].stat.name+" "+data.stats[3].base_stat+" / "+data.stats[4].stat.name+" "+data.stats[4].base_stat+" / "+data.stats[5].stat.name+" "+data.stats[5].base_stat,
+                document.getElementById('tipo').innerHTML=data.types[0].type.name+(", ")+data.types[1].type.name
+            ));
+        break;
     }
 }
